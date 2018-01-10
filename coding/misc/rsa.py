195e15bdd41c42d2f7a642d1eac2120e6522f2db
Luc Sarzyniec <devel@olbat.net>
2018-01-10 17:29:22 +0100

    Improve the RSA script: handle primes of any size and refactoring
    Dynamically determine the block size depending on the size of the modulus,
    handle primes of any size, add functions to dump/load the keys.

--- coding/misc/rsa.py
+++ coding/misc/rsa.py
@@ -7,18 +7,12 @@ modes:
 """
 
 import sys
-import struct
-import random
+from math import ceil
+from io import BytesIO
+from random import sample
 
 PUBKEY_FILE = "key.pub"
 PRIVKEY_FILE = "key.priv"
-INT_SIZE = 8
-PACK_SYM = {
-    1: "B",
-    2: "H",
-    4: "I",
-    8: "Q",
-}
 
 
 def _egcd(b, n):
@@ -48,13 +42,35 @@ def _modinv(b, n):
         return x % n
 
 
+def _bytesize(n):
+    """
+    Get the size (in bytes) necessary to store numbers that are smaller than
+    the modulus (n)
+    """
+    return ceil(n.bit_length() / 8)
+
+
+def dumpkey(n, x):
+    """
+    Dumps an RSA public/private key (modulus _n_ and exponent _x_) as a string
+    """
+    return "{} {}".format(hex(n), hex(x))
+
+
+def loadkey(s):
+    """
+    Loads RSA public/private key modulus and exponent from a string
+    """
+    return [int(v, 16) for v in s.split()]
+
+
 def genkey(primes):
     """
     Generate RSA modulus and public/private exponent
     Random primes numbers are picked from the _primes_ list to simplify
     the implementation
     """
-    p, q = random.sample(primes, 2)
+    p, q = sample(primes, 2)
     n = p * q  # modulus
 
     e = 65537  # public exponent (see https://crypto.stackexchange.com/a/3113)
@@ -71,28 +87,33 @@ def encrypt(m, e, n):
     Encrypt the _m_ message using the public exponent _e_ and the modulus _n_
     """
     m = bytearray(m, "utf-8")
-    r = bytearray(len(m) * INT_SIZE)
-    o = 0
+    s = _bytesize(n)
+    r = BytesIO()  # size = len(m) * s
 
     # handle message byte-per-byte: very spece-inefficient and time consuming
     # but easy to implement and no need for padding
     for b in m:
-        struct.pack_into(PACK_SYM[INT_SIZE], r, o, pow(b, e, n))
-        o += INT_SIZE
-    return bytes(r)
+        v = pow(b, e, n)
+        r.write(v.to_bytes(s, byteorder='little', signed=False))
+
+    return r.getvalue()
 
 
 def decrypt(m, d, n):
     """
     Decrypt the _m_ message using the private exponent _d_ and the modulus _n_
     """
-    m = bytearray(m)
-    r = bytearray(int(len(m) / INT_SIZE))
-    o = 0
-    while o < len(m):
-        b = struct.unpack_from(PACK_SYM[INT_SIZE], m, o)[0]
-        r.append(pow(b, d, n))
-        o += INT_SIZE
+    s = _bytesize(n)
+    if (len(m) % s) != 0:
+        raise ValueError
+    r = bytearray(int(len(m) / s))
+    m = BytesIO(m)
+
+    for b in iter(lambda: m.read(s), b""):
+        v = int.from_bytes(b, byteorder='little', signed=False)
+        b = pow(v, d, n)
+        r.append(b)
+
     return bytes(r)
 
 
@@ -109,11 +130,9 @@ if sys.argv[1] == "genkey":
     e, d, n = genkey(primes)
 
     with open(PUBKEY_FILE, 'w+') as f:
-        f.write(hex(n) + "\n")
-        f.write(hex(e) + "\n")
+        f.write(dumpkey(n, e))
     with open(PRIVKEY_FILE, 'w+') as f:
-        f.write(hex(n) + "\n")
-        f.write(hex(d) + "\n")
+        f.write(dumpkey(n, d))
 
     print("done, wrote keys to {} and {}".format(PUBKEY_FILE, PRIVKEY_FILE))
 
@@ -123,8 +142,7 @@ elif sys.argv[1] == "encrypt":
         sys.exit(1)
 
     with open(sys.argv[2]) as f:
-        n = int(f.readline(), 16)
-        e = int(f.readline(), 16)
+        n, e = loadkey(f.read())
 
     sys.stdout.buffer.write(encrypt(sys.stdin.read(), e, n))
 
@@ -134,8 +152,7 @@ elif sys.argv[1] == "decrypt":
         sys.exit(1)
 
     with open(sys.argv[2]) as f:
-        n = int(f.readline(), 16)
-        d = int(f.readline(), 16)
+        n, d = loadkey(f.read())
 
     sys.stdout.buffer.write(decrypt(sys.stdin.buffer.read(), d, n))
 else:
