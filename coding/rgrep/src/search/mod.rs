91753544394d419d5bdc1f1b6db9b46b451c75c5
Luc Sarzyniec <devel@olbat.net>
2020-08-23 15:38:46 +0200

    [rgrep] Add tests for searching directories in search module

--- coding/rgrep/src/search/mod.rs
+++ coding/rgrep/src/search/mod.rs
@@ -81,6 +81,11 @@ fn search_string(string: &str, pattern: &str) -> bool {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::fs::{self, File};
+    use std::io::Write;
+    use std::io::{Seek, SeekFrom};
+    use tempfile::tempdir;
+    use tempfile::tempfile;
 
     #[test]
     fn simple_search_string() {
@@ -89,4 +94,142 @@ mod tests {
         assert_eq!(search_string(string, "simple"), true);
         assert_eq!(search_string(string, "complex"), false);
     }
+
+    #[test]
+    fn simple_search_file() {
+        let mut file = tempfile().unwrap();
+        writeln!(file, "first simple string");
+        writeln!(file, "other string");
+        writeln!(file, "some other string");
+        writeln!(file);
+        writeln!(file, "second simple string");
+        writeln!(file, "a last string");
+        file.seek(SeekFrom::Start(0)).unwrap();
+
+        let options = Options::new();
+        let mut results = search_file("simple", file, &options);
+
+        let result = results.next();
+        assert!(result.is_some());
+        let result = result.unwrap();
+        assert_eq!(result.0, 1);
+        assert!(result.1.is_ok());
+        assert_eq!(result.1.unwrap(), "first simple string");
+
+        let result = results.next();
+        assert!(result.is_some());
+        let result = result.unwrap();
+        assert_eq!(result.0, 5);
+        assert!(result.1.is_ok());
+        assert_eq!(result.1.unwrap(), "second simple string");
+
+        let result = results.next();
+        assert!(result.is_none());
+    }
+
+    #[test]
+    fn simple_search_paths() {
+        let dir = tempdir().unwrap();
+        let dir_path = dir.path();
+
+        let file1_path = dir_path.join("file1");
+        let mut file1 = File::create(&file1_path).unwrap();
+        writeln!(file1);
+        writeln!(file1, "first simple string");
+        writeln!(file1, "other string");
+        writeln!(file1);
+        writeln!(file1);
+        writeln!(file1, "second simple string");
+
+        let file2_path = dir_path.join("file2");
+        let mut file2 = File::create(&file2_path).unwrap();
+        writeln!(file2, "nothing");
+        writeln!(file2, "matching");
+        writeln!(file2, "here");
+
+        let subdir_path = dir_path.join("some").join("sub").join("dir");
+        fs::create_dir_all(&subdir_path).unwrap();
+
+        let file3_path = subdir_path.join("file3");
+        let mut file3 = File::create(&file3_path).unwrap();
+        writeln!(file3, "third simple string");
+        writeln!(file3, "third simple string");
+        writeln!(file3, "third simple string");
+
+        let paths = vec![&file1_path, &file2_path, &file3_path];
+        let options = Options::new();
+
+        let mut paths_results = search_paths("simple", paths.iter().map(|p| (p, None)), &options);
+
+        let paths_result = paths_results.next();
+        assert!(paths_result.is_some());
+        let paths_result = paths_result.unwrap();
+        assert_eq!(*paths_result.0, &file1_path);
+        assert!(paths_result.1.is_ok());
+
+        // file1
+        let mut path_results = paths_result.1.unwrap();
+        let results = path_results.next();
+        assert!(results.is_some());
+        let result = results.unwrap();
+        assert_eq!(result.0, 2);
+        assert!(result.1.is_ok());
+        assert_eq!(result.1.unwrap(), "first simple string");
+
+        let results = path_results.next();
+        assert!(results.is_some());
+        let result = results.unwrap();
+        assert_eq!(result.0, 6);
+        assert!(result.1.is_ok());
+        assert_eq!(result.1.unwrap(), "second simple string");
+
+        let results = path_results.next();
+        assert!(results.is_none());
+
+        // file2
+        let path_result = paths_results.next();
+        assert!(path_result.is_some());
+        let path_result = path_result.unwrap();
+        assert_eq!(*path_result.0, &file2_path);
+        assert!(path_result.1.is_ok());
+
+        let mut path_results = path_result.1.unwrap();
+        let results = path_results.next();
+        assert!(results.is_none());
+
+        // file3
+        let path_result = paths_results.next();
+        assert!(path_result.is_some());
+        let path_result = path_result.unwrap();
+        assert_eq!(*path_result.0, &file3_path);
+        assert!(path_result.1.is_ok());
+
+        let mut path_results = path_result.1.unwrap();
+        let results = path_results.next();
+        assert!(results.is_some());
+        let result = results.unwrap();
+        assert_eq!(result.0, 1);
+        assert!(result.1.is_ok());
+        assert_eq!(result.1.unwrap(), "third simple string");
+
+        let results = path_results.next();
+        assert!(results.is_some());
+        let result = results.unwrap();
+        assert_eq!(result.0, 2);
+        assert!(result.1.is_ok());
+        assert_eq!(result.1.unwrap(), "third simple string");
+
+        let results = path_results.next();
+        assert!(results.is_some());
+        let result = results.unwrap();
+        assert_eq!(result.0, 3);
+        assert!(result.1.is_ok());
+        assert_eq!(result.1.unwrap(), "third simple string");
+
+        let results = path_results.next();
+        assert!(results.is_none());
+    }
+
+    #[test]
+    fn search_path_with_error() {}
 }
