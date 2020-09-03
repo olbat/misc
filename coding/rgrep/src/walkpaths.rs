2f0ae51a88506e6c3004a9be3fa5cd873ed3ea13
Luc Sarzyniec <devel@olbat.net>
2020-09-03 23:45:58 +0200

    [rgrep] Add tests for walkpath module

--- coding/rgrep/src/walkpaths.rs
+++ coding/rgrep/src/walkpaths.rs
@@ -91,3 +91,82 @@ impl Iterator for WalkPaths {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashSet;
+    use std::fs::{self, File};
+    use std::path::{Path, PathBuf};
+    use tempfile::tempdir;
+
+    #[test]
+    fn simple_walkpath() {
+        let dir = tempdir().unwrap();
+        let dir_path = dir.path();
+
+        let file1_path = dir_path.join("file1");
+        File::create(&file1_path).unwrap();
+
+        let file2_path = dir_path.join("file2");
+        let sub_dir_path = dir_path.join("dir");
+        fs::create_dir_all(&sub_dir_path).unwrap();
+        File::create(&file2_path).unwrap();
+
+        let nested_dir_path = dir_path.join("some").join("nested").join("dir");
+        fs::create_dir_all(&nested_dir_path).unwrap();
+        let file3_path = sub_dir_path.join("file3");
+        File::create(&file3_path).unwrap();
+
+        let empty_dir_path = dir_path.join("some").join("empty").join("dir");
+        fs::create_dir_all(&empty_dir_path).unwrap();
+
+        let paths: HashSet<&Path> = [
+            file1_path.as_ref(),
+            file2_path.as_ref(),
+            file3_path.as_ref(),
+        ]
+        .iter()
+        .cloned()
+        .collect();
+
+        let mut walkpaths = WalkPaths::new(vec![&dir_path]);
+
+        let result = walkpaths.next();
+        assert!(result.is_some());
+        let result = result.unwrap();
+        assert!(paths.contains::<&Path>(&result.0.as_ref()));
+        assert!(result.1.is_none());
+
+        let result = walkpaths.next();
+        assert!(result.is_some());
+        let result = result.unwrap();
+        assert!(paths.contains::<&Path>(&result.0.as_ref()));
+        assert!(result.1.is_none());
+
+        let result = walkpaths.next();
+        assert!(result.is_some());
+        let result = result.unwrap();
+        assert!(paths.contains::<&Path>(&result.0.as_ref()));
+        assert!(result.1.is_none());
+
+        let result = walkpaths.next();
+        assert!(result.is_none());
+    }
+
+    #[cfg(unix)]
+    #[test]
+    fn test_skip_special_devices() {
+        let path = String::from("/dev/null");
+        let paths = vec![&path];
+
+        let mut walkpaths = WalkPaths::new(paths);
+
+        let result = walkpaths.next();
+        assert!(result.is_some());
+        let result = result.unwrap();
+
+        assert_eq!(result.0, PathBuf::from(path));
+        assert!(result.1.is_some());
+    }
+}
